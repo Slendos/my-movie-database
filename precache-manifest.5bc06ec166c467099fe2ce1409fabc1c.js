a4fbfb0b547e0b24f61d39345e6133cb58c2686e
Slendos <slendos123@gmail.com>
2019-05-21 16:00:22 +0200

    Updates

---
+++ precache-manifest.5bc06ec166c467099fe2ce1409fabc1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44437caa82520737bb122ff3c7c874c3",
+    "url": "/Slendos/my-movie-database/index.html"
+  },
+  {
+    "revision": "d6ad96ede9f66eb58cb9",
+    "url": "/Slendos/my-movie-database/static/css/2.3b138234.chunk.css"
+  },
+  {
+    "revision": "c37bafda0f6e32d08939",
+    "url": "/Slendos/my-movie-database/static/css/main.a1004b1b.chunk.css"
+  },
+  {
+    "revision": "d6ad96ede9f66eb58cb9",
+    "url": "/Slendos/my-movie-database/static/js/2.39fc2051.chunk.js"
+  },
+  {
+    "revision": "c37bafda0f6e32d08939",
+    "url": "/Slendos/my-movie-database/static/js/main.290f0f8d.chunk.js"
+  },
+  {
+    "revision": "36538057e96c5fd48c48",
+    "url": "/Slendos/my-movie-database/static/js/runtime~main.0a9b5710.js"
+  },
+  {
+    "revision": "dc170dd519bf1ffdcef56fc11d541679",
+    "url": "/Slendos/my-movie-database/static/media/filmIcon.dc170dd5.png"
+  }
+]);
